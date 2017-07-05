7711cf7f2f601928b2a3fa6f938194cfcdbd41b7
Fernando Serrano Carpena <ferserc1@gmail.com>
2017-07-05 09:37:52 +0200

    Gulpfile.js updated to allow generation of editor files only

--- gulpfile.js
+++ gulpfile.js
@@ -8,57 +8,14 @@ const   gulp = require('gulp'),
 
 var config = {
 	outDir:'build/',
-	paellaDir:'bower_components/paella/player'
-};
-
-gulp.task("webserver", function() {
-	connect.server();
-});
-
-gulp.task("compile", function() {
-	gulp.src(["src/*.js","plugins/*/*.js"])
-		//.pipe(sourcemaps.init())
-		.pipe(traceur())
-		.pipe(concat("paella_editor.js"))
-		//.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(`${config.outDir}player/javascript/`));
-});
-
-gulp.task("compileDebug", function() {
-	gulp.src(["src/*.js","plugins/*/*.js","src-debug/*.js"])
-		//.pipe(sourcemaps.init())
-		.pipe(traceur())
-		.pipe(concat("paella_editor.js"))
-		//.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(`${config.outDir}player/javascript/`));
-});
-
-gulp.task("styles", function() {
-	gulp.src(['style/*.css','plugins/**/*.css'])
-		.pipe(concat('editor.css'))
-		.pipe(gulp.dest(`${config.outDir}player/resources/editor/css/`));
-});
-
-gulp.task("copy", function() {
-	gulp.src(`${config.paellaDir}/**`)
-		.pipe(gulp.dest(`${config.outDir}player`));
-	
-	gulp.src('editor.html')
-		.pipe(gulp.dest(`${config.outDir}player`));
-		
-	gulp.src('templates/**')
-		.pipe(gulp.dest(`${config.outDir}player/templates`));
-	
-	gulp.src('config/editor-config.json')
-		.pipe(gulp.dest(`${config.outDir}player/config`));
-	
-	gulp.src('demos/**')
-		.pipe(gulp.dest(`${config.outDir}repository`));
-		
-	gulp.src('plugins/*/*.html')
-		.pipe(gulp.dest(`${config.outDir}player/templates/`));
-	
-	var depsjs = [
+	editorDir:'player/',
+	buildTest:true,
+	paellaDir:'bower_components/paella/player',
+	editorFiles: [
+		"src/*.js",
+		"plugins/*/*.js"
+	],
+	deps: [
 		'bower_components/angular/angular.min.js',
 		'bower_components/angular/angular.js',
 		'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
@@ -68,35 +25,108 @@ gulp.task("copy", function() {
 		'bower_components/bootstrap/dist/js/bootstrap.min.js',
 		'bower_components/angularjs-slider/dist/rzslider.js',
 		'bower_components/traceur/traceur.min.js'
+	]
+};
+
+gulp.task("configTest", function() {
+	config.outDir = 'build/';
+	config.editorDir = 'player/';
+	config.buildTest = true;
+	config.editorFiles.push("src-debug/*.js");
+});
+
+gulp.task("configAll", function() {
+	config.outDir = 'build/';
+	config.editorDir = 'player/';
+	config.buildTest = false;
+});
+
+gulp.task("configEditorOnly", function() {
+	config.outDir = 'build/editor-files/'
+	config.editorDir = '';
+	config.buildTest = false;
+	config.paellaDir = "";
+});
+
+gulp.task("webserver", function() {
+	return connect.server();
+});
+
+gulp.task("compile", function() {
+	return gulp.src(config.editorFiles)
+		.pipe(traceur())
+		.pipe(concat("paella_editor.js"))
+		.pipe(gulp.dest(`${ config.outDir }${ config.editorDir }javascript/`));
+});
+
+gulp.task("styles", function() {
+	return gulp.src(['style/*.css','plugins/**/*.css'])
+		.pipe(concat('editor.css'))
+		.pipe(gulp.dest(`${ config.outDir }${ config.editorDir }/resources/editor/css/`));
+});
+
+function $p(task) {
+	return new Promise((resolve) => {
+		task.on('end',resolve);
+	});
+}
+
+gulp.task("copy", function() {
+	let promises = [
+		$p(gulp.src('editor.html')
+			.pipe(gulp.dest(`${config.outDir}${ config.editorDir }`))),
+	
+		$p(gulp.src('templates/**')
+			.pipe(gulp.dest(`${config.outDir}${ config.editorDir }templates`))),
+	
+		$p(gulp.src('config/editor-config.json')
+			.pipe(gulp.dest(`${config.outDir}${ config.editorDir }config`))),
+	
+		
+		$p(gulp.src('plugins/*/*.html')
+			.pipe(gulp.dest(`${config.outDir}${ config.editorDir }templates/`))),
+
+		$p(gulp.src(config.deps)
+			.pipe(gulp.dest(`${config.outDir}${ config.editorDir }javascript/`))),
+
+		$p(gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css',
+				'bower_components/angularjs-slider/dist/rzslider.min.css'])
+			.pipe(gulp.dest(`${config.outDir}${ config.editorDir }resources/editor/css`))),
+		
+		$p(gulp.src('bower_components/bootstrap/dist/fonts/*')
+			.pipe(gulp.dest(`${config.outDir}${ config.editorDir }resources/editor/fonts`)))
 	];
-	gulp.src(depsjs)
-		.pipe(gulp.dest(`${config.outDir}player/javascript/`));
-	
-	gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css',
-			  'bower_components/angularjs-slider/dist/rzslider.min.css'])
-		.pipe(gulp.dest(`${config.outDir}player/resources/editor/css`));
-	
-	gulp.src('bower_components/bootstrap/dist/fonts/*')
-		.pipe(gulp.dest(`${config.outDir}player/resources/editor/fonts`));
+
+	if (config.paellaDir) {
+		promises.push($p(gulp.src(`${ config.paellaDir }/**`)
+			.pipe(gulp.dest(`${ config.outDir }${ config.editorDir }`))));
+	}
+
+	if (config.buildTest) {
+		promises.push($p(gulp.src('demos/**')
+			.pipe(gulp.dest(`${config.outDir}repository`))));
+	}
+
+	return Promise.all(promises);
 });
 
 gulp.task('dictionary', function() {
 	function addDictionary(lang) {
-		gulp.src([`localization/*${lang}.json`,`plugins/**/localization/*${lang}.json`])
+		return gulp.src([`localization/*${lang}.json`,`plugins/**/localization/*${lang}.json`])
 			.pipe(merge(`editor_${lang}.json`))
-			.pipe(gulp.dest(`${config.outDir}player/localization/`));
+			.pipe(gulp.dest(`${config.outDir}${ config.editorDir }localization/`));
 	}
 	
-	addDictionary('en');
-	addDictionary('es');
+	return Promise.all([
+		$p(addDictionary('en')),
+		$p(addDictionary('es'))
+	])
 });
 
-
-gulp.task("build", ["compile","styles","dictionary","copy"]);
-gulp.task("buildDebug", ["compileDebug","styles","dictionary","copy"])
+gulp.task("buildFiles", ["compile","styles","dictionary","copy"]);
 
 gulp.task("watch", function() {
-	gulp.watch([
+	return gulp.watch([
 		'editor.html',
 		'templates/**',
 		'demos/**',
@@ -110,5 +140,8 @@ gulp.task("watch", function() {
 	],["buildDebug"]);
 });
 
+gulp.task("build",["configAll","buildFiles"]);
+gulp.task("serve",["configTest","buildFiles","webserver","watch"]);
+gulp.task("editorFiles",["configEditorOnly","buildFiles"]);
+
 gulp.task("default",["build"]);
-gulp.task("serve",["buildDebug","webserver","watch"]);
